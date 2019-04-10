fc953aa65af7b7254ceba377e75702071fcdb824
Alexander Juda <alexander.juda@icloud.com>
2019-04-10 18:53:55 +0200

    Add parse-find test cases

--- test/test_parser_find.py
+++ test/test_parser_find.py
@@ -2,11 +2,22 @@ import src.parser as dp
 import src.clj as clj
 
 
-def test_parse_find1():
+def test_parse_find_rel():
     assert dp.parse_find(clj.str2edn('[?a ?b]')) \
         == dp.FindRel([dp.Variable(clj.S('?a')), dp.Variable(clj.S('?b'))])
 
 
-def test_parse_find2():
+def test_parse_find_coll():
     assert dp.parse_find(clj.str2edn('[[?a ...]]')) \
         == dp.FindColl(dp.Variable(clj.S('?a')))
+
+
+def test_parse_find_scalar():
+    assert dp.parse_find(clj.str2edn('[?a .]')) \
+        == dp.FindScalar(dp.Variable(clj.S('?a')))
+
+
+def test_parse_find_tuple():
+    assert dp.parse_find(clj.str2edn('[[?a ?b]]')) \
+        == dp.FindTuple([dp.Variable(clj.S('?a'), dp.Variable(clj.S('?b')))])
+
