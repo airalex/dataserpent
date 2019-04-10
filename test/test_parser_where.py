9f433093b9645e4b4891c105b5cf8cf7528fa0e9
Alexander Juda <alexander.juda@icloud.com>
2019-04-10 20:35:12 +0200

    Add first test of parsing where

---
+++ test/test_parser_where.py
@@ -0,0 +1,10 @@
+import src.parser as dp
+import src.clj as clj
+
+
+def test_pattern_eav():
+    assert dp.parse_clause(clj.str2edn('[?e ?a ?v]')) \
+        == dp.Pattern(dp.DefaultSrc(),
+                      [dp.Variable(clj.S('?e')),
+                       dp.Variable(clj.S('?a')),
+                       dp.Variable(clj.S('?v'))])
