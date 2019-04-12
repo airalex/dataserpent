c475f19d08f0da43b60c3c4a8f26de3d5dbb240f
Alexander Juda <alexander.juda@icloud.com>
2019-04-13 00:42:43 +0200

    Add parse-in tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -58,3 +58,27 @@ def test_bindings_vec_vec_a_b_c():
 def test_in_x():
     assert dp.parse_in(clj.str2edn('[?x]')) \
         == [dp.BindScalar(dp.Variable(clj.S('?x')))]
+
+
+def test_in_scalars():
+    assert dp.parse_in(clj.str2edn('[$ $1 % _ ?x]')) \
+        == [dp.BindScalar(dp.SrcVar(clj.S('$'))),
+            dp.BindScalar(dp.SrcVar(clj.S('$1'))),
+            dp.BindScalar(dp.RulesVar(None)),
+            dp.BindIgnore(None),
+            dp.BindScalar(dp.Variable(clj.S('?x')))]
+
+
+def test_in_coll_scalars():
+    assert dp.parse_in(clj.str2edn('[$ [[_ [?x ...]] ...]]')) \
+        == [dp.BindScalar(dp.SrcVar(clj.S('$'))),
+            dp.BindColl(dp.BindTuple([dp.BindIgnore(None),
+                                      dp.BindColl(
+                                          dp.BindScalar(dp.Variable(clj.S('?x'))))]))]
+
+
+def test_in_x_key():
+    clause = clj.str2edn('[?x :key]')
+    with pytest.raises(AssertionError) as excinfo:
+        dp.parse_in(clause)
+    assert str(excinfo.value) == "Cannot parse binding, expected (bind-scalar | bind-tuple | bind-coll | bind-rel)"
